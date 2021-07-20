d27e99e724b09e524ddc07e885f21f473c39d915
Felipe Borba <felipe.borba0109@gmail.com>
2021-07-19 21:55:39 -0300

    fix: problem of connection in mongodb databse api_data

--- src/repository/mongo/db.js
+++ src/repository/mongo/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-  const username = "root";
-  const password = "pass12345";
-  const uri = `mongodb://${username}:${password}@localhost:27017/`;
+  const uri = 'mongodb://localhost:27017/api_data'
   return await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    auth: {
+      "authSource": "admin"
+    },
+    user: "root",
+    pass: "pass12345"
   });
 }
 
